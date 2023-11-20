2aee24143102311c0aa56e8a8a1368b08f23fe7d
sprbkyrssl <151548729+sprbkyrssl@users.noreply.github.com>
2023-11-20 21:18:32 +0600

    Update and rename README.md to final.py

---
+++ final.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from cloudipsp import Api, Checkout
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apple.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+#text = db.Column(db.Text, nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
